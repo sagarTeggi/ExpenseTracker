d0de46af9cb3bb96f5f3f32147dc0576c4c9ac93
sagar <sagar.teggi97@gmail.com>
2024-07-28 21:10:49 +0530

    Added DBContext, Models.

---
+++ ExpenseTracker/DBContext/DBContext.cs
@@ -0,0 +1,17 @@
+﻿using ExpenseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.DBContext
+{
+    public class DBContext : DbContext
+    {
+        
+        public DBContext(DbContextOptions options) : base(options) 
+        {
+        }
+
+        public DbSet<Category> Category { get; set; }
+
+
+    }
+}
---
+++ ExpenseTracker/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpenseTracker.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string? CategoryName { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public DateTime UpdateTime { get; set; }
+    }
+}
